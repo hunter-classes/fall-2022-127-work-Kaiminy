0d0f157a36d49097f4be4094187f814dafd5c780
Kaiminy <104949057+Kaiminy@users.noreply.github.com>
2022-12-12 17:17:02 +0000

    added main.cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,27 @@
+//hello world
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+}
+
+// if and loop
+y= 9
+
+if (y=9) {
+  cout<<"You are 19!\n";
+}
+else if (y=0){
+  cout<<"You are not 19";
+}
+
+
+// function
+}
+
+int add_two_numbers(int a, int b){
+  int c;
+  std::cout << "In func a: " << a << " b: " << b << "\n";
+  c = a + b;
+  return c;
+}
